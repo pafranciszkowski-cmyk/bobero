ada636eb66a43c737a597fc390fa89f30dc962e8
pafranciszkowski-cmyk <pa.franciszkowski@gmail.com>
2026-07-31 02:40:39 +0200

    Dodaj Vercel Web Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import { Analytics } from "@vercel/analytics/react";
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(
+    <>
+        <App />
+        <Analytics />
+    </>
+  );
